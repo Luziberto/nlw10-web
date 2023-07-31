a8c495cbb14eaefefe9f7803ba1cf929f510c27f
Luziberto Mendes <luzibertomendesjunior@hotmail.com>
2023-07-30 23:28:18 -0300

    Aula-02

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif'
+      },
       backgroundImage: {
+        app: 'url(/app-bg.png)',
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        yellow: {
+          500: '#F7DD43',
+          700: '#E5CD3D',
+        },
+        ignite: {
+          500: '#129E57'
+        },
+        gray: {
+          100: '#E1E1E6',
+          300: '#8D8D99',
+          600: '#323238',
+          800: '#202024',
+          900: '#121214'
+        }
+      }
     },
   },
   plugins: [],
